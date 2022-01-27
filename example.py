5510f4bbdeef7f6e0db597d438c919bca0527e11
thearyadev <87589047+thearyadev@users.noreply.github.com>
2022-01-27 08:44:12 -0500

    Add files via upload

---
+++ example.py
@@ -0,0 +1,7 @@
+from utils import Console
+
+if __name__ == '__main__':
+    console = Console()
+    console.info_log("Hello World")
+    console.server_log("Hello World")
+    console.error_log("Hello World")
---
+++ utils/__init__.py
@@ -0,0 +1,23 @@
+from .console import Console
+
+MAP_LIST = (
+    "Volskaya",
+    "Lijiang",
+    "Rialto",
+    "Kings Row",
+    "Eichenwalde",
+    "Dorado",
+    "Hollywood",
+    "Havana",
+    "Ilios",
+    "Gibraltar",
+    "Junkertown",
+    "Oasis",
+    "Blizzard World",
+    "Anubis",
+    "Busan",
+    "Hanamura",
+    "Numbani",
+    "Route 66",
+    "Nepal"
+)
